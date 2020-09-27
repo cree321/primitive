013cdf13fcd72302c7c86e262c7785fd39a62059
cree321 <42984414+cree321@users.noreply.github.com>
2020-09-26 23:30:13 -0700

    change subject

--- scripts/spheretrace.js
+++ scripts/spheretrace.js
@@ -1,337 +1,178 @@
-if (!navigator.gpu || GPUBufferUsage.COPY_SRC === undefined)
-      document.body.className = 'error';
-
-    const numSamples = 1500;
-
-    const renderShadersWHLSL = `
-vertex float4 vertex_main(float2 particlePos : attribute(0), float2 particleVel : attribute(1), float2 position : attribute(2)) : SV_Position
-{
-    float angle = -atan(particleVel.x / particleVel.y);
-    float2 result = float2(position.x * cos(angle) - position.y * sin(angle),
-        position.x * sin(angle) + position.y * cos(angle));
-    return float4(particlePos, 0, 1);
-}
-
-fragment float4 fragment_main() : SV_Target 0
-{
-    return float4(1.0, 1.0, 1.0, 1.0);
-}
-`;
-
-    const computeShaderWHLSL = `
-struct Particle {
-    float2 pos;
-    float2 vel;
-}
-
-struct SimParams {
-    float deltaT;
-    float rule1Distance;
-    float rule2Distance;
-    float rule3Distance;
-    float rule1Scale;
-    float rule2Scale;
-    float rule3Scale;
-}
-
-[numthreads(1, 1, 1)]
-compute void compute_main(constant SimParams[] paramsBuffer : register(b0), device Particle[] particlesA : register(u1), device Particle[] particlesB : register(u2), float3 threadID : SV_DispatchThreadID) {
-    uint index = uint(threadID.x);
-
-    SimParams params = paramsBuffer[0];
-
-    if (index >= ${numSamples}) { 
+async function helloTriangle() {
+    if (!navigator.gpu || GPUBufferUsage.COPY_SRC === undefined) {
+        document.body.className = 'error';
         return;
     }
 
-    float2 vPos = particlesA[index].pos;
-    float2 vVel = particlesA[index].vel;
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+    
+    /*** Shader Setup ***/
+    
+    /* GPUShaderModule */
+    const positionLocation = 0;
+    const colorLocation = 1;
 
-    float2 cMass = float2(0.0, 0.0);
-    float2 cVel = float2(0.0, 0.0);
-    float2 colVel = float2(0.0, 0.0);
-    float cMassCount = 0.0;
-    float cVelCount = 0.0;
-
-    float2 pos;
-    float2 vel;
-    for (uint i = 0; i < ${numParticles}; ++i) {
-        if (i == index) { continue; }
-        pos = particlesA[i].pos.xy;
-        vel = particlesA[i].vel.xy;
-
-        if (distance(pos, vPos) < params.rule1Distance) {
-            cMass += pos;
-            cMassCount++;
-        }
-        if (distance(pos, vPos) < params.rule2Distance) {
-            colVel -= (pos - vPos);
-        }
-        if (distance(pos, vPos) < params.rule3Distance) {
-            cVel += vel;
-            cVelCount++;
-        }
-    }
-    if (cMassCount > 0.0) {
-        cMass = cMass / cMassCount - vPos;
-    }
-    if (cVelCount > 0.0) {
-        cVel = cVel / cVelCount;
+    const whlslSource = `
+    struct FragmentData {
+        float4 position : SV_Position;
+        float4 color : attribute(${colorLocation});
     }
 
-    vVel += cMass * params.rule1Scale + colVel * params.rule2Scale + cVel * params.rule3Scale;
-
-    // clamp velocity for a more pleasing simulation.
-    vVel = normalize(vVel) * clamp(length(vVel), 0.0, 0.1);
-
-    // kinematic update
-    vPos += vVel * params.deltaT;
-
-    // Wrap around boundary
-    if (vPos.x < -1.0) vPos.x = 1.0;
-    if (vPos.x > 1.0) vPos.x = -1.0;
-    if (vPos.y < -1.0) vPos.y = 1.0;
-    if (vPos.y > 1.0) vPos.y = -1.0;
-
-    particlesB[index].pos = vPos;
-    particlesB[index].vel = vVel;
-}
-`;
-
-async function init() {
-  const adapter = await navigator.gpu.requestAdapter();
-  const device = await adapter.requestDevice();
-
-  const canvas = document.querySelector('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  const context = canvas.getContext('gpu');
-
-  const swapChain = context.configureSwapChain({
-    device,
-    format: "bgra8unorm"
-  });
-
-  const computeBindGroupLayout = device.createBindGroupLayout({
-    bindings: [
-      { binding: 0, visibility: GPUShaderStage.COMPUTE, type: "uniform-buffer" },
-      { binding: 1, visibility: GPUShaderStage.COMPUTE, type: "storage-buffer" },
-      { binding: 2, visibility: GPUShaderStage.COMPUTE, type: "storage-buffer" },
-    ],
-  });
-
-  const computePipelineLayout = device.createPipelineLayout({
-    bindGroupLayouts: [computeBindGroupLayout],
-  });
-
-  device.pushErrorScope('validation');
-
-  const renderModule = device.createShaderModule({ code: renderShadersWHLSL, isWHLSL: true });
-
-  const renderPipeline = device.createRenderPipeline({
-        layout: device.createPipelineLayout({ bindGroupLayouts: [] }),
-
-        vertexStage: {
-          module: renderModule,
-          entryPoint: "vertex_main"
-        },
-        fragmentStage: {
-          module: renderModule,
-          entryPoint: "fragment_main"
-        },
-
-        primitiveTopology: "triangle-list",
-
-        depthStencilState: {
-          depthWriteEnabled: true,
-          depthCompare: "less",
-          format: "depth32float-stencil8",
-          stencilFront: {},
-          stencilBack: {},
-        },
-
-        vertexInput: {
-          indexFormat: "uint32",
-          vertexBuffers: [{
-            // instanced particles buffer
-            stride: 4 * 4,
-            stepMode: "instance",
-            attributeSet: [{
-              // instance position
-              shaderLocation: 0,
-              offset: 0,
-              format: "float2"
-            }, {
-              // instance velocity
-              shaderLocation: 1,
-              offset: 2 * 4,
-              format: "float2"
-            }],
-          }, {
-            // vertex buffer
-            stride: 2 * 4,
-            stepMode: "vertex",
-            attributeSet: [{
-              // vertex positions
-              shaderLocation: 2,
-              offset: 0,
-              format: "float2"
-            }],
-          }],
-        },
-
-        rasterizationState: {
-          frontFace: 'ccw',
-          cullMode: 'none',
-        },
-
-        colorStates: [{
-          format: "bgra8unorm",
-          alphaBlend: {},
-          colorBlend: {},
-        }],
-      });
-
-      device.popErrorScope().then(error => {
-        if (error)
-          console.error("Render shaders: " + error.message);
-      });
-
-      device.pushErrorScope('validation');
-
-      const computePipeline = device.createComputePipeline({
-        layout: computePipelineLayout,
-        computeStage: {
-          module: device.createShaderModule({
-            code: computeShaderWHLSL, isWHLSL: true
-          }),
-          entryPoint: "compute_main",
-        }
-      });
-
-      device.popErrorScope().then(error => {
-        if (error)
-          console.error("Compute shader: " + error.message);
-      });
-
-      const depthTexture = device.createTexture({
-        size: { width: canvas.width, height: canvas.height, depth: 1 },
-        arrayLayerCount: 1,
-        mipLevelCount: 1,
-        sampleCount: 1,
-        dimension: "2d",
-        format: "depth32float-stencil8",
-        usage: GPUTextureUsage.OUTPUT_ATTACHMENT
-      });
-
-      const renderPassDescriptor = {
-        colorAttachments: [{
-          loadOp: "clear",
-          clearColor: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-          storeOp: "store",
-        }],
-        depthStencilAttachment: {
-          attachment: depthTexture.createDefaultView(),
-          depthLoadOp: "clear",
-          depthStoreOp: "store",
-          clearDepth: 1.0,
-          stencilLoadValue: 0,
-          stencilStoreOp: "store",
-        }
-      };
-
-      const vertexBufferData = new Float32Array([-0.01, -0.02, 0.01, -0.02, 0.00, 0.02]);
-      const [verticesBuffer, verticesArrayBuffer] = device.createBufferMapped({
-        size: vertexBufferData.byteLength,
-        usage: GPUBufferUsage.VERTEX,
-      });
-      new Float32Array(verticesArrayBuffer).set(vertexBufferData);
-      verticesBuffer.unmap();
-
-      const simParamData = new Float32Array([0.04, 0.1, 0.025, 0.025, 0.02, 0.05, 0.005]);
-      const [simParamBuffer, simParamArrayBuffer] = device.createBufferMapped({
-        size: simParamData.byteLength,
-        usage: GPUBufferUsage.UNIFORM,
-      });
-      new Float32Array(simParamArrayBuffer).set(simParamData);
-      simParamBuffer.unmap();
-
-      const initialParticleData = new Float32Array(numParticles * 4);
-      for (let i = 0; i < numParticles; ++i) {
-        initialParticleData[4 * i + 0] = 2 * (Math.random() - 0.5);
-        initialParticleData[4 * i + 1] = 2 * (Math.random() - 0.5);
-        initialParticleData[4 * i + 2] = 2 * (Math.random() - 0.5) * 0.1;
-        initialParticleData[4 * i + 3] = 2 * (Math.random() - 0.5) * 0.1;
-      }
-
-      const particleBuffers = new Array(2);
-      const particleBindGroups = new Array(2);
-      for (let i = 0; i < 2; ++i) {
-        let particleArrayBuffer;
-        [particleBuffers[i], particleArrayBuffer] = device.createBufferMapped({
-          size: initialParticleData.byteLength,
-          usage: GPUBufferUsage.VERTEX | GPUBufferUsage.STORAGE
-        });
-        new Float32Array(particleArrayBuffer).set(initialParticleData);
-        particleBuffers[i].unmap();
-      }
-    for (let i = 0; i < 2; ++i) {
-      particleBindGroups[i] = device.createBindGroup({
-        layout: computeBindGroupLayout,
-        bindings: [
-          {
-            binding: 0,
-            resource: {
-              buffer: simParamBuffer,
-              offset: 0,
-              size: simParamData.byteLength
-            },
-          },
-          {
-          binding: 1,
-          resource: {
-            buffer: particleBuffers[i],
-            offset: 0,
-            size: initialParticleData.byteLength,
-          },
-        },
-        {
-          binding: 2,
-          resource: {
-            buffer: particleBuffers[(i + 1) % 2],
-            offset: 0,
-            size: initialParticleData.byteLength,
-          },
-        }
-      ],
-    });
-  }
-
-  let t = 0;
-  function frame() {
-    renderPassDescriptor.colorAttachments[0].attachment = swapChain.getCurrentTexture().createDefaultView();
-
-    const commandEncoder = device.createCommandEncoder({});
-      {
-        const passEncoder = commandEncoder.beginComputePass();
-        passEncoder.setPipeline(computePipeline);
-        passEncoder.setBindGroup(0, particleBindGroups[t % 2]);
-        passEncoder.dispatch(numParticles, 1, 1);
-        passEncoder.endPass();
-      }
+    vertex FragmentData vertexMain(float4 position : attribute(${positionLocation}), float4 color : attribute(${colorLocation}))
     {
-      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-      passEncoder.setPipeline(renderPipeline);
-      passEncoder.setVertexBuffers(0, [particleBuffers[(t + 1) % 2], verticesBuffer], [0, 0]);
-      passEncoder.draw(3, numParticles, 0, 0);
-      passEncoder.endPass();
-    }
-    device.getQueue().submit([commandEncoder.finish()]);
+        FragmentData out;
 
-    ++t;
-    requestAnimationFrame(frame);
-  }
-  requestAnimationFrame(frame);
+        out.position = position;
+        out.color = color;
+
+        return out;
+    }
+
+    fragment float4 fragmentMain(float4 color : attribute(${colorLocation})) : SV_Target 0
+    {
+        return color;
+    }
+    `;
+    const shaderModule = device.createShaderModule({ code: whlslSource, isWHLSL: true });
+    
+    /* GPUPipelineStageDescriptors */
+    const vertexStageDescriptor = { module: shaderModule, entryPoint: "vertexMain" };
+    const fragmentStageDescriptor = { module: shaderModule, entryPoint: "fragmentMain" };
+    
+    /*** Vertex Buffer Setup ***/
+    
+    /* Vertex Data */
+    const colorOffset = 4 * 4; // 4 floats of 4 bytes each.
+    const vertexStride = 8 * 4;
+    const vertexDataSize = vertexStride * 3;
+    
+    /* GPUBufferDescriptor */
+    const vertexDataBufferDescriptor = { 
+        size: vertexDataSize,
+        usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.VERTEX
+    };
+    /* GPUBuffer */
+    const vertexBuffer = device.createBuffer(vertexDataBufferDescriptor);
+    
+    /*** Write Data To GPU ***/
+    
+    const vertexArrayBuffer = await vertexBuffer.mapWriteAsync();
+    const vertexWriteArray = new Float32Array(vertexArrayBuffer);
+    vertexWriteArray.set([
+        // x, y, z, w, r, g, b, a
+        0, 0.8, 0, 1, 0, 1, 1, 1,
+        -0.8, -0.8, 0, 1, 1, 1, 0, 1,
+        0.8, -0.8, 0, 1, 1, 0, 1, 1
+    ]);
+    vertexBuffer.unmap();
+    
+    /*** Describe Vertex Data For Pipeline ***/
+    
+    const vertexBufferSlot = 0;
+    
+    /* GPUVertexAttributeDescriptors */
+    const positionAttribute = {
+        shaderLocation: positionLocation,
+        offset: 0,
+        format: "float4"
+    };
+    const colorAttribute = {
+        shaderLocation: colorLocation,
+        offset: colorOffset,
+        format: "float4"
+    };
+
+    /* GPUVertexBufferDescriptor */
+    const vertexBufferDescriptor = {
+        stride: vertexStride,
+        attributeSet: [positionAttribute, colorAttribute]
+    };
+
+    /* GPUVertexInputDescriptor */
+    const vertexInputDescriptor = {
+        vertexBuffers: [vertexBufferDescriptor]
+    };
+    
+    /*** Finish Pipeline State ***/
+    
+    /* GPUBlendDescriptors */
+    const alphaBlendDescriptor = { srcFactor: "one", dstFactor: "zero", operation: "add" };
+    const colorBlendDescriptor = { srcFactor: "one", dstFactor: "zero", operation: "add" };
+    
+    /* GPUColorStateDescriptor */
+    const colorStateDescriptor = {
+        format: "bgra8unorm",
+        alphaBlend: alphaBlendDescriptor,
+        colorBlend: colorBlendDescriptor,
+        writeMask: GPUColorWrite.ALL
+    };
+    
+    /* GPURenderPipelineDescriptor */
+    const renderPipelineDescriptor = {
+        vertexStage: vertexStageDescriptor,
+        fragmentStage: fragmentStageDescriptor,
+        primitiveTopology: "triangle-list",
+        colorStates: [colorStateDescriptor],
+        vertexInput: vertexInputDescriptor
+    };
+    /* GPURenderPipeline */
+    const renderPipeline = device.createRenderPipeline(renderPipelineDescriptor);
+    
+    /*** Swap Chain Setup ***/
+    
+    const canvas = document.querySelector("canvas");
+    canvas.width = 600;
+    canvas.height = 600;
+
+    const gpuContext = canvas.getContext("gpu");
+    
+    /* GPUSwapChainDescriptor */
+    const swapChainDescriptor = { device: device, format: "bgra8unorm" };
+    /* GPUSwapChain */
+    const swapChain = gpuContext.configureSwapChain(swapChainDescriptor);
+    
+    /*** Render Pass Setup ***/
+    
+    /* Acquire Texture To Render To */
+    
+    /* GPUTexture */
+    const swapChainTexture = swapChain.getCurrentTexture();
+    /* GPUTextureView */
+    const renderAttachment = swapChainTexture.createDefaultView();
+    
+    /* GPUColor */
+    const darkBlue = { r: 0.15, g: 0.15, b: 0.5, a: 1 };
+    
+    /* GPURenderPassColorATtachmentDescriptor */
+    const colorAttachmentDescriptor = {
+        attachment: renderAttachment,
+        loadOp: "clear",
+        storeOp: "store",
+        clearColor: darkBlue
+    };
+    
+    /* GPURenderPassDescriptor */
+    const renderPassDescriptor = { colorAttachments: [colorAttachmentDescriptor] };
+    
+    /*** Rendering ***/
+    
+    /* GPUCommandEncoder */
+    const commandEncoder = device.createCommandEncoder();
+    /* GPURenderPassEncoder */
+    const renderPassEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    
+    renderPassEncoder.setPipeline(renderPipeline);
+    renderPassEncoder.setVertexBuffers(vertexBufferSlot, [vertexBuffer], [0]);
+    renderPassEncoder.draw(3, 1, 0, 0); // 3 vertices, 1 instance, 0th vertex, 0th instance.
+    renderPassEncoder.endPass();
+    
+    /* GPUComamndBuffer */
+    const commandBuffer = commandEncoder.finish();
+    
+    /* GPUQueue */
+    const queue = device.getQueue();
+    queue.submit([commandBuffer]);
 }
 
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener("DOMContentLoaded", helloTriangle);
