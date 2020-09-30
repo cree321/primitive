017075b29ee369e9b9c88579c7cb1849a10818ce
cree321 <42984414+cree321@users.noreply.github.com>
2020-09-29 22:30:58 -0700

    Update spheretrace.js

--- scripts/spheretrace.js
+++ scripts/spheretrace.js
@@ -1,176 +1,344 @@
 async function helloTriangle() {
+const threadsPerThreadgroup = 32;
+
+const sourceBufferBindingNum = 0;
+const outputBufferBindingNum = 1;
+const uniformsBufferBindingNum = 2;
+
+// Enough space to store 1 radius and 33 weights.
+const maxUniformsSize = (32 + 2) * Float32Array.BYTES_PER_ELEMENT;
+
+let image, context2d, device;
+
+const width = 60;
+
+async function init() {
     if (!navigator.gpu || GPUBufferUsage.COPY_SRC === undefined) {
-        document.body.className = 'error';
+        document.body.className = "error";
         return;
     }
 
-    const adapter = await navigator.gpu.requestAdapter();
-    const device = await adapter.requestDevice();
-    
-    /*** Shader Setup ***/
-    
-    /* GPUShaderModule */
-    const positionLocation = 0;
-    const colorLocation = 1;
-
-    const whlslSource = `
-    struct FragmentData {
-        float4 position : SV_Position;
-        float4 color : attribute(${colorLocation});
-    }
-    vertex FragmentData vertexMain(float4 position : attribute(${positionLocation}), float4 color : attribute(${colorLocation}))
-    {
-        FragmentData out;
-        out.position = position;
-        out.color = color;
-        return out;
-    }
-    fragment float4 fragmentMain(float4 color : attribute(${colorLocation})) : SV_Target 0
-    {
-      return color;
-    }
-    `;
-    const shaderModule = device.createShaderModule({ code: whlslSource, isWHLSL: true });
-    
-    /* GPUPipelineStageDescriptors */
-    const vertexStageDescriptor = { module: shaderModule, entryPoint: "vertexMain" };
-    const fragmentStageDescriptor = { module: shaderModule, entryPoint: "fragmentMain" };
-    
-    /*** Vertex Buffer Setup ***/
-    
-    /* Vertex Data */
-    const colorOffset = 4 * 4; // 4 floats of 4 bytes each.
-    const vertexStride = 8 * 4;
-    const vertexDataSize = vertexStride * 3;
-    
-    /* GPUBufferDescriptor */
-    const vertexDataBufferDescriptor = { 
-        size: vertexDataSize,
-        usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.VERTEX
-    };
-    /* GPUBuffer */
-    const vertexBuffer = device.createBuffer(vertexDataBufferDescriptor);
-    
-    /*** Write Data To GPU ***/
-    
-    const vertexArrayBuffer = await vertexBuffer.mapWriteAsync();
-    const vertexWriteArray = new Float32Array(vertexArrayBuffer);
-    vertexWriteArray.set([
-        // x, y, z, w, r, g, b, a
-        -0.9, 0.9, 0, 1, 0, 1, 1, 1,
-        -0.9, -0.9, 0, 1, 1, 1, 0, 1,
-        1.9, 0.9, 0, 1, 1, 0, 1, 1
-    ]);
-    vertexBuffer.unmap();
-    
-    /*** Describe Vertex Data For Pipeline ***/
-    
-    const vertexBufferSlot = 0;
-    
-    /* GPUVertexAttributeDescriptors */
-    const positionAttribute = {
-        shaderLocation: positionLocation,
-        offset: 0,
-        format: "float4"
-    };
-    const colorAttribute = {
-        shaderLocation: colorLocation,
-        offset: colorOffset,
-        format: "float4"
-    };
-
-    /* GPUVertexBufferDescriptor */
-    const vertexBufferDescriptor = {
-        stride: vertexStride,
-        attributeSet: [positionAttribute, colorAttribute]
-    };
-
-    /* GPUVertexInputDescriptor */
-    const vertexInputDescriptor = {
-        vertexBuffers: [vertexBufferDescriptor]
-    };
-    
-    /*** Finish Pipeline State ***/
-    
-    /* GPUBlendDescriptors */
-    const alphaBlendDescriptor = { srcFactor: "one", dstFactor: "zero", operation: "add" };
-    const colorBlendDescriptor = { srcFactor: "one", dstFactor: "zero", operation: "add" };
-    
-    /* GPUColorStateDescriptor */
-    const colorStateDescriptor = {
-        format: "bgra8unorm",
-        alphaBlend: alphaBlendDescriptor,
-        colorBlend: colorBlendDescriptor,
-        writeMask: GPUColorWrite.ALL
-    };
-    
-    /* GPURenderPipelineDescriptor */
-    const renderPipelineDescriptor = {
-        vertexStage: vertexStageDescriptor,
-        fragmentStage: fragmentStageDescriptor,
-        primitiveTopology: "triangle-list",
-        colorStates: [colorStateDescriptor],
-        vertexInput: vertexInputDescriptor
-    };
-    /* GPURenderPipeline */
-    const renderPipeline = device.createRenderPipeline(renderPipelineDescriptor);
-    
-    /*** Swap Chain Setup ***/
-    
+    const slider = document.querySelector("input");
     const canvas = document.querySelector("canvas");
-    canvas.width = 60;
-    canvas.height = 60;
+    canvas.width = width;
+    canvas.height = width;
+    context2d = canvas.getContext("2d");
 
-    const gpuContext = canvas.getContext("gpu");
-    
-    /* GPUSwapChainDescriptor */
-    const swapChainDescriptor = { device: device, format: "bgra8unorm" };
-    /* GPUSwapChain */
-    const swapChain = gpuContext.configureSwapChain(swapChainDescriptor);
-    
-    /*** Render Pass Setup ***/
-    
-    /* Acquire Texture To Render To */
-    
-    /* GPUTexture */
-    const swapChainTexture = swapChain.getCurrentTexture();
-    /* GPUTextureView */
-    const renderAttachment = swapChainTexture.createDefaultView();
-    
-    /* GPUColor */
-    const darkBlue = { r: 0.15, g: 0.15, b: 0.5, a: 1 };
-    
-    /* GPURenderPassColorATtachmentDescriptor */
-    const colorAttachmentDescriptor = {
-        attachment: renderAttachment,
-        loadOp: "clear",
-        storeOp: "store",
-        clearColor: darkBlue
+    const adapter = await navigator.gpu.requestAdapter();
+    device = await adapter.requestDevice();
+    image = new Image(canvas.width, canvas.height);
+    context2d.drawImage(image, 0, 0);
+
+    setUpCompute();
+
+    let busy = false;
+    let inputQueue = [];
+    slider.oninput = async () => {
+        inputQueue.push(slider.value);
+        
+        if (busy)
+            return;
+
+        busy = true;
+        while (inputQueue.length != 0)
+            await computeBlur(inputQueue.shift());
+        busy = false;
     };
-    
-    /* GPURenderPassDescriptor */
-    const renderPassDescriptor = { colorAttachments: [colorAttachmentDescriptor] };
-    
-    /*** Rendering ***/
-    
-    /* GPUCommandEncoder */
-    const commandEncoder = device.createCommandEncoder();
-    /* GPURenderPassEncoder */
-    const renderPassEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-    
-    renderPassEncoder.setPipeline(renderPipeline);
-    renderPassEncoder.setVertexBuffers(vertexBufferSlot, [vertexBuffer], [0]);
-    renderPassEncoder.draw(3, 1, 0, 0); // 3 vertices, 1 instance, 0th vertex, 0th instance.
-    renderPassEncoder.endPass();
-    
-    /* GPUComamndBuffer */
-    const commandBuffer = commandEncoder.finish();
-    
-    /* GPUQueue */
-    const queue = device.getQueue();
-    queue.submit([commandBuffer]);
-    
-    alert(whlslSource);
 }
 
-//window.addEventListener("DOMContentLoaded", helloTriangle);
+let originalData, imageSize;
+let originalBuffer, storageBuffer, resultsBuffer, uniformsBuffer;
+let horizontalBindGroup, verticalBindGroup, horizontalPipeline, verticalPipeline;
+
+function setUpCompute() {
+    originalData = context2d.getImageData(0, 0, image.width, image.height);
+    imageSize = originalData.data.length;
+
+    // Buffer creation
+    let originalArrayBuffer;
+    [originalBuffer, originalArrayBuffer] = device.createBufferMapped({ size: imageSize, usage: GPUBufferUsage.STORAGE });
+    const imageWriteArray = new Uint8ClampedArray(originalArrayBuffer);
+    imageWriteArray.set(originalData.data);
+    originalBuffer.unmap();
+
+    storageBuffer = device.createBuffer({ size: imageSize, usage: GPUBufferUsage.STORAGE });
+    resultsBuffer = device.createBuffer({ size: imageSize, usage: GPUBufferUsage.STORAGE | GPUBufferUsage.MAP_READ });
+    uniformsBuffer = device.createBuffer({ size: maxUniformsSize, usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.MAP_WRITE });
+
+    // Bind buffers to kernel   
+    const bindGroupLayout = device.createBindGroupLayout({
+        bindings: [{
+            binding: sourceBufferBindingNum,
+            visibility: GPUShaderStage.COMPUTE,
+            type: "storage-buffer"
+        }, {
+            binding: outputBufferBindingNum,
+            visibility: GPUShaderStage.COMPUTE,
+            type: "storage-buffer"
+        }, {
+            binding: uniformsBufferBindingNum,
+            visibility: GPUShaderStage.COMPUTE,
+            type: "uniform-buffer"
+        }]
+    });
+
+    horizontalBindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        bindings: [{
+            binding: sourceBufferBindingNum,
+            resource: {
+                buffer: originalBuffer,
+                size: imageSize
+            }
+        }, {
+            binding: outputBufferBindingNum,
+            resource: {
+                buffer: storageBuffer,
+                size: imageSize
+            }
+        }, {
+            binding: uniformsBufferBindingNum,
+            resource: {
+                buffer: uniformsBuffer,
+                size: maxUniformsSize
+            }
+        }]
+    });
+
+    verticalBindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        bindings: [{
+            binding: sourceBufferBindingNum,
+            resource: {
+                buffer: storageBuffer,
+                size: imageSize
+            }
+        }, {
+            binding: outputBufferBindingNum,
+            resource: {
+                buffer: resultsBuffer,
+                size: imageSize
+            }
+        }, {
+            binding: uniformsBufferBindingNum,
+            resource: {
+                buffer: uniformsBuffer,
+                size: maxUniformsSize
+            }
+        }]
+    });
+
+    // Set up pipelines
+    const pipelineLayout = device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout] });
+
+    const shaderModule = device.createShaderModule({ code: createShaderCode(image), isWHLSL: true });
+
+    horizontalPipeline = device.createComputePipeline({ 
+        layout: pipelineLayout, 
+        computeStage: {
+            module: shaderModule,
+            entryPoint: "horizontal"
+        }
+    });
+
+    verticalPipeline = device.createComputePipeline({
+        layout: pipelineLayout,
+        computeStage: {
+            module: shaderModule,
+            entryPoint: "vertical"
+        }
+    });
+}
+
+async function computeBlur(radius) {
+    if (radius == 0) {
+        context2d.drawImage(image, 0, 0, width, width);
+        return;
+    }
+    const setUniformsPromise = setUniforms(radius);
+    const uniformsMappingPromise = uniformsBuffer.mapWriteAsync();
+
+    const [uniforms, uniformsArrayBuffer] = await Promise.all([setUniformsPromise, uniformsMappingPromise]);
+
+    const uniformsWriteArray = new Float32Array(uniformsArrayBuffer);
+    uniformsWriteArray.set(uniforms);
+    uniformsBuffer.unmap();
+
+    // Run horizontal pass first
+    const commandEncoder = device.createCommandEncoder();
+    const passEncoder = commandEncoder.beginComputePass();
+    passEncoder.setBindGroup(0, horizontalBindGroup);
+    passEncoder.setPipeline(horizontalPipeline);
+    const numXGroups = Math.ceil(image.width / threadsPerThreadgroup);
+    passEncoder.dispatch(numXGroups, image.height, 1);
+    passEncoder.endPass();
+
+    // Run vertical pass
+    const verticalPassEncoder = commandEncoder.beginComputePass();
+    verticalPassEncoder.setBindGroup(0, verticalBindGroup);
+    verticalPassEncoder.setPipeline(verticalPipeline);
+    const numYGroups = Math.ceil(image.height / threadsPerThreadgroup);
+    verticalPassEncoder.dispatch(image.width, numYGroups, 1);
+    verticalPassEncoder.endPass();
+
+    device.getQueue().submit([commandEncoder.finish()]);
+
+    // Draw resultsBuffer as imageData back into context2d
+    const resultArrayBuffer = await resultsBuffer.mapReadAsync();
+    const resultArray = new Uint8ClampedArray(resultArrayBuffer);
+    context2d.putImageData(new ImageData(resultArray, image.width, image.height), 0, 0);
+    resultsBuffer.unmap();
+}
+
+window.addEventListener("load", init);
+
+/* Helpers */
+
+let uniformsCache = new Map();
+
+async function setUniforms(radius)
+{
+    let uniforms = uniformsCache.get(radius);
+    if (uniforms != undefined)
+        return uniforms;
+
+    const sigma = radius / 2.0;
+    const twoSigma2 = 2.0 * sigma * sigma;
+
+    uniforms = [radius];
+    let weightSum = 0;
+
+    for (let i = 0; i <= radius; ++i) {
+        const weight = Math.exp(-i * i / twoSigma2);
+        uniforms.push(weight);
+        weightSum += (i == 0) ? weight : weight * 2;
+    }
+
+    // Compensate for loss in brightness
+    const brightnessScale =  1 - (0.1 / 32.0) * radius;
+    weightSum *= brightnessScale;
+    for (let i = 1; i < uniforms.length; ++i)
+        uniforms[i] /= weightSum;
+        
+    uniformsCache.set(radius, uniforms);
+
+    return uniforms;
+}
+
+const byteMask = (1 << 8) - 1;
+
+function createShaderCode(image) {
+    return `
+uint getR(uint rgba)
+{
+    return rgba & ${byteMask};
+}
+
+uint getG(uint rgba)
+{
+    return (rgba >> 8) & ${byteMask};
+}
+
+uint getB(uint rgba)
+{
+    return (rgba >> 16) & ${byteMask};
+}
+
+uint getA(uint rgba)
+{
+    return (rgba >> 24) & ${byteMask};
+}
+
+uint makeRGBA(uint r, uint g, uint b, uint a)
+{
+    return r + (g << 8) + (b << 16) + (a << 24);
+}
+
+void accumulateChannels(thread uint[] channels, uint startColor, float weight)
+{
+    channels[0] += uint(float(getR(startColor)) * weight);
+    channels[1] += uint(float(getG(startColor)) * weight);
+    channels[2] += uint(float(getB(startColor)) * weight);
+    channels[3] += uint(float(getA(startColor)) * weight);
+
+    // Compensate for brightness-adjusted weights.
+    if (channels[0] > 255)
+        channels[0] = 255;
+
+    if (channels[1] > 255)
+        channels[1] = 255;
+
+    if (channels[2] > 255)
+        channels[2] = 255;
+
+    if (channels[3] > 255)
+        channels[3] = 255;
+}
+
+uint horizontallyOffsetIndex(uint index, int offset, int rowStart, int rowEnd)
+{
+    int offsetIndex = int(index) + offset;
+
+    if (offsetIndex < rowStart || offsetIndex >= rowEnd)
+        return index;
+    
+    return uint(offsetIndex);
+}
+
+uint verticallyOffsetIndex(uint index, int offset, uint length)
+{
+    int realOffset = offset * ${image.width};
+    int offsetIndex = int(index) + realOffset;
+
+    if (offsetIndex < 0 || offsetIndex >= int(length))
+        return index;
+    
+    return uint(offsetIndex);
+}
+
+[numthreads(${threadsPerThreadgroup}, 1, 1)]
+compute void horizontal(constant uint[] source : register(u${sourceBufferBindingNum}),
+                        device uint[] output : register(u${outputBufferBindingNum}),
+                        constant float[] uniforms : register(b${uniformsBufferBindingNum}),
+                        float3 dispatchThreadID : SV_DispatchThreadID)
+{
+    int radius = int(uniforms[0]);
+    int rowStart = ${image.width} * int(dispatchThreadID.y);
+    int rowEnd = ${image.width} * (1 + int(dispatchThreadID.y));
+    uint globalIndex = uint(rowStart) + uint(dispatchThreadID.x);
+
+    uint[4] channels;
+
+    for (int i = -radius; i <= radius; ++i) {
+        uint startColor = source[horizontallyOffsetIndex(globalIndex, i, rowStart, rowEnd)];
+        float weight = uniforms[uint(abs(i) + 1)];
+        accumulateChannels(@channels, startColor, weight);
+    }
+
+    output[globalIndex] = makeRGBA(channels[0], channels[1], channels[2], channels[3]);
+}
+
+[numthreads(1, ${threadsPerThreadgroup}, 1)]
+compute void vertical(constant uint[] source : register(u${sourceBufferBindingNum}),
+                        device uint[] output : register(u${outputBufferBindingNum}),
+                        constant float[] uniforms : register(b${uniformsBufferBindingNum}),
+                        float3 dispatchThreadID : SV_DispatchThreadID)
+{
+    int radius = int(uniforms[0]);
+    uint globalIndex = uint(dispatchThreadID.x) * ${image.height} + uint(dispatchThreadID.y);
+
+    uint[4] channels;
+
+    for (int i = -radius; i <= radius; ++i) {
+        uint startColor = source[verticallyOffsetIndex(globalIndex, i, source.length)];
+        float weight = uniforms[uint(abs(i) + 1)];
+        accumulateChannels(@channels, startColor, weight);
+    }
+
+    output[globalIndex] = makeRGBA(126, channels[1], channels[2], channels[3]);
+}
+`;
+}
+}
+
+window.addEventListener("DOMContentLoaded", helloTriangle);
